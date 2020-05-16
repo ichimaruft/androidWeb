cb41923235460ac63fd6015dcec477334bd9fd0c
dennis <fangtao@qianshengqian.com>
2020-05-16 21:49:07 +0800

    改成he

--- app/src/main/java/com/example/h5/MainActivity.kt
+++ app/src/main/java/com/example/h5/MainActivity.kt
@@ -9,10 +9,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("dennis","la")
-        Log.d("dennis","la")
-        Log.d("dennis","la")
-        Log.d("dennis","la")
-        Log.d("dennis","la")
+        Log.d("dennis","he")
+        Log.d("dennis","he")
+        Log.d("dennis","he")
+        Log.d("dennis","he")
+        Log.d("dennis","he")
     }
 }
